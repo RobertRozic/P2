5de1e4a61e2d3f981f178dccfa567ebc8b33516f
Robert Rozic <r.rozic97@gmail.com>
2022-04-28 13:49:52 +0200

    Setovi i ponavljanje

---
+++ 04_Setovi_i_ponavljanje/setovi.py
@@ -0,0 +1,10 @@
+voce = { "jabuka", "banana", "sljiva", "jabuka" }
+#voce = [ "jabuka", "banana", "sljiva", "jabuka" ]
+print(voce)
+print(type(voce))
+print(len(voce))
+
+#voce.add("tresnja")
+voce.discard("tresnja")
+print(voce)
+
