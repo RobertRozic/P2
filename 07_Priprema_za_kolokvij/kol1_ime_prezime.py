f4ed7b0e299b65b7f2e2f1ec2fd8864b17154510
RobertRozic <r.rozic97@gmail.com>
2024-05-14 14:24:57 +0200

    Priprema za kolokvij

---
+++ 07_Priprema_za_kolokvij/kol1_ime_prezime.py
@@ -0,0 +1,44 @@
+from countries import countries
+import random
+
+finalisti_uzorak = random.sample(countries, 26)
+finalisti = []
+
+for drzava, izvodjac, pjesma in finalisti_uzorak:
+    el = {
+        "drzava": drzava,
+        "izvodjac": izvodjac,
+        "pjesma": pjesma
+    }
+    finalisti.append(el)
+
+bodovi = [12, 10, 8, 7, 6, 5, 4, 3, 2, 1]
+for drzava, izvodjac, pjesma in countries:
+    bez_drzave = []
+    for item in finalisti:
+        if item["drzava"] != drzava:
+            bez_drzave.append(item)
+    za_bodovanje = random.sample(bez_drzave, 10)
+    brojac = 0
+    for bod in bodovi:
+        naziv_drzave = za_bodovanje[brojac]["drzava"]
+        for finalist in finalisti:
+            if finalist["drzava"] == naziv_drzave:
+                finalist["bodovi"] = finalist.get("bodovi", 0) + bod
+                break
+        brojac += 1
+
+finalisti = sorted(finalisti, key=lambda x: x['bodovi'], reverse=True)
+
+bodovi = 0
+for item in finalisti:
+    bodovi += item["bodovi"]
+
+print(bodovi)
+
+import re
+reg = "$.{10}";
+
+for item in finalisti:
+	if re.search(reg, item['pjesma']):
+		print(item)
