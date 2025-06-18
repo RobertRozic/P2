da3f9e1086cb55a2069df78221a1c3df7de88168
RobertRozic <r.rozic97@gmail.com>
2025-06-18 17:49:42 +0200

    Primjeri kolokvija

---
+++ Primjeri_kolokvija/kol2/primjer.py
@@ -0,0 +1,13 @@
+'''
+Potrebno je napraviti 3 funckije u 1 datoteci funkcije.py
+
+I. Napisati  generator funkciju koja prima dva parametra (interval) za ispis svih brojeva djeljivih s 3 u tom intervalu.
+
+II. Rekurzivna funkcija za izračunavanje n-te potencije broja i objašnjenje u komentaru što je rekurzija i na koji način funckionira navedena funkcija.
+
+III. Napisati lambda funkciju za ispis kuba broja (potencija na 3).
+
+U novoj skripti učitaj sve tri funkcije modularno, jednu od njih kao alias.
+Od korisnika zatražiti unos broja i pozvati I. generator funkciju. Ispisati sve brojeve koje vraća generator funkcija.
+Od korisnika zatražiti unos broja koji će se kubirati i pozvati II. i III. funkciju, usporedi jesu li dobiveni rezultati jednaki.
+'''
