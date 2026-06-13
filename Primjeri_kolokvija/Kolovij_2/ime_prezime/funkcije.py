a7cc551b94a04b57a928c11c655c84b43c3e8b2c
RobertRozic <r.rozic97@gmail.com>
2026-06-13 16:15:26 +0200

    Primjer 2. kolokvija

---
+++ Primjeri_kolokvija/Kolovij_2/ime_prezime/funkcije.py
@@ -0,0 +1,19 @@
+'''
+Potrebno je napisati 3 funckije u 1 datoteci funkcije.py
+
+I.
+Napiši generator funkciju koja prima dva cijela broja (start i end) i vraća sve brojeve iz tog intervala koji su djeljivi s 5.
+
+II.
+Napiši rekurzivnu funkciju koja prima broj n i vraća sumu svih brojeva od 1 do n.
+U komentaru objasni što je rekurzija i kako funkcija radi.
+
+III.
+Napiši lambda funkciju koja prima broj i vraća njegov kvadrat.
+
+U novoj skripti učitaj sve tri funkcije modularno, jednu od njih kao alias.
+Od korisnika zatražiti unos dva broja, granice intervala i pozvati I. generator funkciju. Ispisati sve brojeve koje vraćaa generator funkcija.
+Iz niza brojeva koje je generirala funckija I. izaberi nasumična 2 broja. Jedan proslijedi u funkciju II. a drugi u funkciju III. i ispisi rezultate.
+
+Rješenje učitati kao .zip file naziva ime_prezime.zip
+'''
